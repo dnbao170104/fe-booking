69cd5d8541cc97be21fd7b3c29b87a5ffb21a5e5
Doan Ngoc Bao <dnbao123@gmail.com>
2026-04-16 12:55:10 +0700

    fix api fe

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://be-quan-ly-booking-ks-1.onrender.com",
+  // Đã thêm /api vào cuối đường link
+  baseURL: "https://be-quan-ly-booking-ks-1.onrender.com/api",
   timeout: 15000,
 });
 
