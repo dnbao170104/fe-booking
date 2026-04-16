82df380c89d458c62ad58655995a62371b450d77
Doan Ngoc Bao <dnbao123@gmail.com>
2026-04-16 12:41:53 +0700

    fix url_be

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://be-quan-ly-booking-ks-1.onrender.com",
   timeout: 15000,
 });
 
